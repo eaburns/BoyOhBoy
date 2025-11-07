cf2a48b1ff2857c0a9afe9e748153dd3d7609d3e
Ethan Burns <burns.ethan@gmail.com>
2025-11-07 10:54:02 -0500

    In gameboy_print_diff, print numbers in hex too.

--- gameboy.c
+++ gameboy.c
@@ -16,9 +16,9 @@ void fail(const char *fmt, ...) {
 
 bool gameboy_eq(const Gameboy *a, const Gameboy *b) {
   return memcmp(a->cpu.registers, b->cpu.registers, sizeof(a->cpu.registers)) ==
-             0 && a->cpu.flags == b->cpu.flags &&
-         a->cpu.sp == b->cpu.sp && a->cpu.pc == b->cpu.pc &&
-         a->cpu.ir == b->cpu.ir &&
+             0 &&
+         a->cpu.flags == b->cpu.flags && a->cpu.sp == b->cpu.sp &&
+         a->cpu.pc == b->cpu.pc && a->cpu.ir == b->cpu.ir &&
          (a->cpu.bank == b->cpu.bank ||
           a->cpu.bank == NULL && b->cpu.bank == instructions ||
           a->cpu.bank == instructions && b->cpu.bank == NULL) &&
@@ -30,7 +30,8 @@ bool gameboy_eq(const Gameboy *a, const Gameboy *b) {
 void gameboy_print_diff(FILE *f, const Gameboy *a, const Gameboy *b) {
   for (int i = 0; i < sizeof(a->cpu.registers); i++) {
     if (a->cpu.registers[i] != b->cpu.registers[i]) {
-      fprintf(f, "registers[%s]: %d != %d\n", reg8_name(i), a->cpu.registers[i],
+      fprintf(f, "registers[%s]: %d ($%02x) != %d ($%02x) \n", reg8_name(i),
+              a->cpu.registers[i], a->cpu.registers[i], b->cpu.registers[i],
               b->cpu.registers[i]);
     }
   }
@@ -41,14 +42,16 @@ void gameboy_print_diff(FILE *f, const Gameboy *a, const Gameboy *b) {
     fprintf(f, "sp: %d != %d\n", a->cpu.sp, b->cpu.sp);
   }
   if (a->cpu.pc != b->cpu.pc) {
-    fprintf(f, "pc: %d != %d\n", a->cpu.pc, b->cpu.pc);
+    fprintf(f, "pc: %d ($%02x) != %d ($%02x)\n", a->cpu.pc, a->cpu.pc, b->cpu.pc,
+            b->cpu.pc);
   }
   if (a->cpu.ir != b->cpu.ir) {
-    fprintf(f, "ir: %d != %d\n", a->cpu.ir, b->cpu.ir);
+    fprintf(f, "ir: %d ($%02x) != %d ($%02x)\n", a->cpu.ir, a->cpu.ir, b->cpu.ir,
+            b->cpu.ir);
   }
   if (!(a->cpu.bank == b->cpu.bank ||
-          a->cpu.bank == NULL && b->cpu.bank == instructions ||
-          a->cpu.bank == instructions && b->cpu.bank == NULL)) {
+        a->cpu.bank == NULL && b->cpu.bank == instructions ||
+        a->cpu.bank == instructions && b->cpu.bank == NULL)) {
     fprintf(f, "bank: %p != %p\n", a->cpu.bank, b->cpu.bank);
   }
   if (a->cpu.cycle != b->cpu.cycle) {
@@ -56,13 +59,14 @@ void gameboy_print_diff(FILE *f, const Gameboy *a, const Gameboy *b) {
   }
   for (int i = 0; i < sizeof(a->cpu.scratch); i++) {
     if (a->cpu.scratch[i] != b->cpu.scratch[i]) {
-      fprintf(f, "scratch[%d]: %d != %d\n", i, a->cpu.scratch[i],
-              b->cpu.scratch[i]);
+      fprintf(f, "scratch[%d]: %d ($%02x) != %d ($%02x)\n", i, a->cpu.scratch[i],
+              a->cpu.scratch[i], b->cpu.scratch[i], b->cpu.scratch[i]);
     }
   }
   for (int i = 0; i < sizeof(a->mem); i++) {
     if (a->mem[i] != b->mem[i]) {
-      fprintf(f, "mem[$%04x]: %d != %d\n", i, a->mem[i], b->mem[i]);
+      fprintf(f, "mem[$%04x]: %d ($%02x) != %d ($%02x)\n", i, a->mem[i], a->mem[i],
+              b->mem[i], b->mem[i]);
     }
   }
 }
